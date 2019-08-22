5710cb4fdb38a8d2c8b6d6a9896f3c8e6028dac5
Filip Kaźmierczak <filip.kazmierczak@amadeus.com>
2019-08-22 14:49:01 +0200

    started project

--- src/components/Ingredients/IngredientForm.js
+++ src/components/Ingredients/IngredientForm.js
@@ -1,21 +1,15 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import Card from '../UI/Card';
-import './IngredientForm.css';
+import Card from "../UI/Card";
+import "./IngredientForm.css";
 
 const IngredientForm = React.memo(props => {
-  const inputState = useState({
-    title: '',
-    amount: ''
-  });
+  const [ enteredTitle, setEnteredTitle] = useState('');
+  const [ enteredAmount, setEnteredAmount] = useState('');
   const submitHandler = event => {
     event.preventDefault();
-    // ...
+    props.onAddIngredient({title: enteredTitle, amount: enteredAmount})
   };
-  const inputChangeHandler = (event, id) => {
-      event.preventDefault();
-      console.log(id)
-  }
 
   return (
     <section className="ingredient-form">
@@ -23,11 +17,27 @@ const IngredientForm = React.memo(props => {
         <form onSubmit={submitHandler}>
           <div className="form-control">
             <label htmlFor="title">Name</label>
-            <input type="text" id="title" value={inputState[0].title} onChange={(event) => inputChangeHandler(this.id, event)}/>
+            <input
+              type="text"
+              id="title"
+              value={enteredTitle}
+              onChange={event => {
+                return setEnteredTitle(event.target.value)
+              }
+            }
+            />
           </div>
           <div className="form-control">
             <label htmlFor="amount">Amount</label>
-            <input type="number" id="amount" value={inputState[0].amount} onChange={(event) => inputChangeHandler(event, this.id)}/>
+            <input
+              type="number"
+              id="amount"
+              value={enteredAmount}
+              onChange={event => {
+                return setEnteredAmount(event.target.value)
+              }
+              }
+            />
           </div>
           <div className="ingredient-form__actions">
             <button type="submit">Add Ingredient</button>
--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
 function Ingredients() {
+  const [userIngredients, setUserIngredients] = useState([]);
+
+  const addIngedientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}]);
+  }
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngedientHandler}/>
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList ingredients={userIngredients}/>
       </section>
     </div>
   );
